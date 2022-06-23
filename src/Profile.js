d4c05268eac0a90406b411ae1b51531324d8b3e0
Syedfahed <102272866+Syedfahed@users.noreply.github.com>
2022-06-23 10:08:20 +0530

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -7,7 +7,7 @@ function Profile() {
     <div className='center'>
         <img className='profilePic' src={img} alt="img"/> <br></br>
        <h4>Hi I’m Syed Motesim Fahed</h4>
-I'm a Professional Front-end Developer. I’ve been working with HTML5, CSS3, Bootstrap, Javascript, Jquery, Ajax, WordPress, React, Php, MySql, Asp Dot Net and SQL server.
+I'm a Professional Front-end Developer. I’ve been working with HTML5, CSS3, Bootstrap, Javascript, Jquery, Ajax, React, Php and mongodb compass server.
 Always up for opportunities where I can show my abilities to solve problems and Bring ease in people's lives.
  <br></br>Will make your imaginations come true😊 
         <br></br>
