0c4e760c2812b7b905894f01ab611fb342ff5197
Syedfahed <102272866+Syedfahed@users.noreply.github.com>
2022-06-23 10:11:16 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -14,7 +14,7 @@ export default function Project() {
             <div className="card-body">
               <h5 className="card-title">Text Edit</h5>
               <p className="card-text">
-                With Text Edit can you change uppercase to lowercase and count
+                With Text Edit you can change uppercase to lowercase and calculate
                 reading time
               </p>
               <a
@@ -32,7 +32,7 @@ export default function Project() {
               <h5 className="card-title">Sport News</h5>
               <p className="card-text">
                 This API base project. You can see live Sport news. This project
-                is available in my githug account. use above link to see{" "}
+                is available in my githug account. use below link to see{" "}
               </p>
               <a href="https://github.com/Syedfahed/sportnews" className="btn btn-primary">
                 click
@@ -46,7 +46,7 @@ export default function Project() {
               <h5 className="card-title">Todo List</h5>
               <p className="card-text">
                 By Entring any word you can add in list. by one click list will
-                underline. with double click delete
+                underline. with double click deleted
               </p>
               <a
                 href="https://todo-app-beta-wine.vercel.app/"
