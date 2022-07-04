e87e39d65df95189fec41d6041fe45a53e248b53
Syedfahed <102272866+Syedfahed@users.noreply.github.com>
2022-07-04 18:28:02 +0530

    ten commit

--- src/components/Email.js
+++ src/components/Email.js
@@ -13,15 +13,15 @@ export default function Email() {
     <h2 className='center'>Get In Touch</h2>
     <form className='form' onSubmit={sendEmail}>
         <label>Name</label>
-        <input placeholder='username' type="name" name="name" className='form-control'/>
+        <input inputMode='text' placeholder='username'  name="name" className='form-control'></input>
         <label>Email</label>
-        <input placeholder='username@gmail.com' type="email" name="user_email" className='form-control'/>
+        <input placeholder='username@gmail.com'  name="user_email" className='form-control'/>
         <label>Message</label>
         <textarea placeholder="Hello I'm......." name='message' rows="4" />
         <button className='btn form-btn' id='form-btn'>Send</button>
+        
     </form> 
 </div>
-
 </>
    )
 }
